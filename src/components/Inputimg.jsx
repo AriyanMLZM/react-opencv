823afce597dc66a8d39d712261721c1ebc96f5ea
AriyanMlzm <ryanmolazem@gmail.com>
2024-04-24 19:29:10 +0330

    add file picker

--- src/components/Inputimg.jsx
+++ src/components/Inputimg.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Inputimg = () => {
+const Inputimg = ({ props }) => {
   return (
-    <section>
-
+    <section className="w-[200px] pb-2 h-[250px] border-[2px] rounded mt-5 flex flex-col items-center">
+      <input
+        id="file-upload"
+        type="file"
+        accept="image/*"
+        className="w-[90%] h-[20%] pt-2 text-[13px] text-white file:hover:bg-primary file:bg-bgColor file:border-[2px] file:border-primary file:border-solid file:px-2 file:text-primary file:hover:text-bgColor file:rounded-full file:mr-2"
+        onChange={(e) => {
+          props.setFile(e.target.files[0])
+        }}
+      />
+      <div className="h-[80%] p-2 flex justify-center items-center text-white overflow-hidden">
+        {props.img && <img src={props.img} alt="No Image" />}
+        {!props.img && <p>No Image Selected</p>}
+      </div>
     </section>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-import { Header } from '../components'
+import { Header, Inputimg } from '../components'
+import { reader } from '../helpers'
 
 const Home = () => {
+  const [file, setFile] = useState(null)
+  const [img, setImg] = useState(null)
+
+  useEffect(() => {
+    if (file) {
+      reader(file).then((result) => setImg(result))
+    }
+  }, [file])
+
   return (
-    <main className='h-screen w-screen bg-bgColor'>
+    <main className="h-screen w-screen bg-bgColor flex items-center flex-col">
       <Header />
+      <Inputimg props={{ img, file, setFile }} />
     </main>
   )
 }
